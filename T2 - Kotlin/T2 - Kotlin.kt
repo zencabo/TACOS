dafde740d0b83b567edc9ddbf4e994ae3bacfbd6
zencabo <cbustamanteo1800@alumno.ipn.mx>
2023-09-10 14:31:11 -0600

    Se agrego elemento de prueba desde android studio

---
+++ T2 - Kotlin/T2 - Kotlin.kt
@@ -0,0 +1,13 @@
+package `T2 - Kotlin`
+
+class T2_Kotlin {
+
+}
+
+fun main() {
+    println("Hello, world!!!")
+    print("Esto es una prueba, Cristian Alain Bustamante Osorio")
+    println("- Abajo se han colocado tres enter")
+    print("-\n-\n-")
+
+}
